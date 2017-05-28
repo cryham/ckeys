d225fb4fce53e7094d0dae28e06a396e5732d259
CryHam <cryham@gmail.com>
2017-05-28 22:15:01 +0200

    keyboard draw, array for key colors, test if in kll

--- source/App_Graph.cpp
+++ source/App_Graph.cpp
@@ -20,10 +20,22 @@ void App::Graph()
 	}
 
 
-	//  draw keyboard  --------
+	//  draw keyboard
+	//--------------------------------
 	const float sc = set.fScale;
 	const int xl = !set.bList ? 20 :  // left margin x
 					set.bListSimple ? 110 : 200;
+	struct rgb{
+		uint8_t r,g,b;
+	};
+	//  [function], [pressed/normal], [rect,frame,text]
+	const static rgb kc[2][2][3] = {{
+		{{ 77,115,153},{204,235,255},{227,242,255}},  // cyan
+		{{ 26, 51, 77},{ 51,102,153},{179,217,254}}},{
+	//	{{ 95,115,163},{215,235,255},{222,242,255}},  // steel
+	//	{{ 35, 51, 87},{ 82,102,163},{197,217,254}}}};
+		{{105,115,163},{225,235,255},{222,242,255}},  // viol blue
+		{{ 45, 51, 87},{ 92,102,163},{197,217,254}}}};
 	xMax = 0;  yMax = 0;
 
 	if (set.bLayout)
@@ -33,22 +45,26 @@ void App::Graph()
 			x2 = x + k.w * sc, y2 = y + k.h * sc;
 
 		//  key
-		if (k.on)
-		{	//  pressed
-			Rect( x, y, x2, y2, 77,115,153);
-			Frame(x, y, x2, y2, 1, 204,235,255);
-			Clr(227,242,255);
-		}else
-		{	//  normal
-			Rect( x, y, x2, y2, 26,51,77);
-			Frame(x, y, x2, y2, 1, 51,102,153);
-			Clr(179,217,254);
-		}
+		int f = set.bKLL ? (k.inKll? 0: 1) :
+				set.bVK ? (k.inKll? 0: 1) : 0;  // test
+		int o = k.on? 0: 1;
+		const rgb* c;
+		c = &kc[f][o][0];  Rect( x, y, x2, y2,    c->r,c->g,c->b);
+		c = &kc[f][o][1];  Frame(x, y, x2, y2, 1, c->r,c->g,c->b);
+		c = &kc[f][o][2];  Clr(c->r,c->g,c->b);
 
 		//  label
-		str = k.s;
+		str = set.bKLL ? k.sk : k.s;
 		text.setCharacterSize(k.sc * sc);
 		Txt(x + 4, y + 4);
+
+		//  layer label
+//		if (set.bL2 && k.hasL2)
+		#if 0
+		str = "a";
+		Clr(140,250,60);
+		Txt(x + 4 + 10.5f*sc, y + 4 + set.iFontH);
+		#endif
 
 		//  get max size
 		if (x2 > xMax)  xMax = x2;
@@ -56,7 +72,8 @@ void App::Graph()
 	}
 
 
-	//  keys pressed list  --------
+	//  keys pressed list
+	//--------------------------------
 	if (!set.bList)  return;
 
 	#ifdef _WIN32
